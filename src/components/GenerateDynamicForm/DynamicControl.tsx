7fb18b5d77893f0f2b0cb15cc16572ef3571cd13
mubin86 <rahman.mmubinur@gmail.com>
2022-12-21 02:09:20 +0600

    initialized dynamic form and made generic component

---
+++ src/components/GenerateDynamicForm/DynamicControl.tsx
@@ -0,0 +1,63 @@
+
+import { useFormContext } from "react-hook-form";
+import { DynamicFieldData } from "./dynamic-control-types";
+import { Input, Select } from 'antd';
+
+// input, textarea, select, a { outline: none; }
+
+// input:focus {
+//     outline:none !important;
+// }
+
+export const DynamicControl = ({
+  inputType,
+  fieldName,
+  defaultValue,
+  options = [],
+  config = {}
+}: DynamicFieldData) => {
+  const { register } = useFormContext();
+
+  switch (inputType) {
+    case "text":
+      return (
+        <input
+          id={fieldName}
+          style={{width: "250px", borderRadius: "9px", padding: "8px", boxShadow: 'none', outline: "0px solid transparent",  border: "1px solid #DCDCDC"  }}
+          type="text"
+          {...register(fieldName, config)}
+          defaultValue={defaultValue}
+        />
+      );
+    case "select": {
+      return (
+        <select
+          {...register(fieldName, config)}
+          defaultValue={defaultValue}
+          name={fieldName}
+            style={{ width: "200px", padding: "8px"}}
+     
+          id={fieldName}
+        >
+          {options.map((o, index) => (
+            <option key={index} value={o.value}>
+              {o.label}
+            </option>
+          ))}
+        </select>
+      );
+    }
+    case "number":
+      return (
+        <input
+          type="number"
+          id={fieldName}
+          style={{width: "250px", borderRadius: "9px", padding: "8px",  outline: "0px solid transparent", border: "1px solid #DCDCDC" }}
+          {...register(fieldName, config)}
+          defaultValue={defaultValue}
+        />
+      );
+    default:
+      return <input type="text" />;
+  }
+};
--- src/pages/admin/application/index.tsx
+++ src/pages/admin/application/index.tsx
@@ -2,15 +2,19 @@ import dynamic from "next/dynamic";
 import _ from "lodash";
 import React, { useState } from "react";
 import useBaseHook from "@src/hooks/BaseHook";
-import { Card, Col, Form, Row, Statistic } from "antd";
+import { Card, Col, Row, Statistic } from "antd";
 import OverviewForm from "@root/src/components/Admin/Application/OverviewForm";
+import { Form } from "../../../components/GenerateDynamicForm/Form";
+import JsonSchema from "../../../../config/Application_schema.json";
+import { DynamicFieldData } from "../../../components/GenerateDynamicForm/dynamic-control-types";
 
 const Layout = dynamic(() => import("@src/layouts/Admin"), { ssr: false });
 
 const Index = () => {
   const [loading, setLoading] = useState(false);
-  const [form] = Form.useForm();
+  // const [form] = Form.useForm();
   const [data, setData] = useState({});
+  const fields: DynamicFieldData[] = JsonSchema.schema.map(item => JSON.parse(JSON.stringify(item)));
 
   const onFinish = async (values: any): Promise<void> => {
     values = {...values,...data}
@@ -22,6 +26,9 @@ const Index = () => {
   return (
     <div className="content">
       <OverviewForm />
+      <h1 style={{marginTop: 5}}>Personal Detail</h1>
+      
+      <Form fields={fields} />
     </div>
   );
 };
